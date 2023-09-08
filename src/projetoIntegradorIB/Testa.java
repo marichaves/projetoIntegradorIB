410f2ac351e94fcee888ab164a242b275b5ccbd3
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-09-07 23:45:44 -0300

    Atualizar o Testa.java

--- src/projetoIntegradorIB/Testa.java
+++ src/projetoIntegradorIB/Testa.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Testa {
 
 	public static void main(String[] args) {
+
+//
 		Conexao conexao = new Conexao();
 		conexao.conectar();
 //		conexao.adicionarDadosConsole();
